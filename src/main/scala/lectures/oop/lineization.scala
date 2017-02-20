e26821a3e69983263cd335bcb2cd2704c1102153
s.popov2@tcsbank.ru <s.popov2@tcsbank.ru>
2017-02-20 14:51:46 +0300

    Lineization enhanced; Typo fixed;

--- src/main/scala/lectures/oop/lineization.scala
+++ src/main/scala/lectures/oop/lineization.scala
@@ -1,52 +1,59 @@
 package lectures.oop
 
 abstract class AbstractTest {
-  //val name: String = ""
-  //println("AbstractTest " + name)
+  val name: String = ""
+  println("Deepest level of inheritance: AbstractTest")
 
-  def whoAmI() = println("AbstractTest")
+  def whoAmI()
 }
 
 class NameProvider extends AbstractTest {
 
-  //override val name: String = "name provided by trait"
-
-  //println("NameProvider name " + name)
+  override val name: String = "name provided by trait"
+  println("One level up: NameProvider")
 
   override def whoAmI() = {
-    println("NameProvider")
-    super.whoAmI()
+    println(s"name is `$name` in NameProvider")
   }
 }
 
 trait AnotherNameProvider extends AbstractTest {
-  //override val name: String = "name provided by another trait"
 
-  //println("AnotherName Provider " + name)
+  override val name: String = "name provided by another trait"
+  println("Almost on the surface: AnotherNameProvider")
 
-  override def whoAmI() = {
-    println("AnotherNameProvider")
+  abstract override def whoAmI() = {
+    println(s"name is `$name` in AnotherNameProvider")
     super.whoAmI()
   }
 }
 
 class ConcreteClass extends NameProvider with AnotherNameProvider {
 
-  //override val name: String = "Concrete class"
-
-  //println("ConcreteClass name " + name)
+  override val name: String = "Concrete class"
+  println("The uppermost level: ConcreteClass")
 
   override def whoAmI() = {
-    println("ConcreteClass")
+    println(s"name is `$name` in Concrete class")
     super.whoAmI()
   }
-
 }
 
 object InheritanceTest extends App {
   val k = new ConcreteClass()
   k.whoAmI()
+  val n = k.name
 }
+
+/*
+Deepest level of inheritance: AbstractTest
+One level up: NameProvider
+Almost on the surface: AnotherNameProvider
+The uppermost level: ConcreteClass
+name is `Concrete class` in Concrete class
+name is `Concrete class` in AnotherNameProvider
+name is `Concrete class` in NameProvider
+*/
 
 
 
--- src/main/scala/lectures/types/FixCompile.scala
+++ src/main/scala/lectures/types/FixCompile.scala
@@ -6,16 +6,16 @@ package lectures.types
   *
   * Ваша задача:
   * Раскоментируйте код
-  * Исправьте тело метода mapper так, чтобы программа скопмилировалась
+  * Исправьте тело метода mapper так, чтобы программа скомпилировалась
   */
 object FixCompile extends App {
 
-  //val mapper = (i: String) => if (i % 2 != 0) i * 2
-
-  //val result = List(1, 2, 3, 4, 5, 6, 7, 8, 9).map {
-  //    mapper
-  //  }.foldLeft(0) { (acc, v) => acc + v }
-
-  // print(result)
+//  val mapper = (i: String) => if (i % 2 != 0) i * 2
+//
+//  val result = List(1, 2, 3, 4, 5, 6, 7, 8, 9).map {
+//      mapper
+//    }.foldLeft(0) { (acc, v) => acc + v }
+//
+//   print(result)
 }
 
